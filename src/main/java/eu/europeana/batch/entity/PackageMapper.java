dbf3bfdc489b66fa589861171a427ad64accf221
Immanuel Kattey <immanuel.kattey@europeana.eu>
2021-11-20 11:32:49 +0100

    Fix comment in PackageMapper

--- src/main/java/eu/europeana/batch/entity/PackageMapper.java
+++ src/main/java/eu/europeana/batch/entity/PackageMapper.java
@@ -1,6 +1,6 @@
 package eu.europeana.batch.entity;
 
 /**
- * Empty interface to be when configuring a Morphia data store to map this package.
+ * Empty interface to use when configuring a Morphia data store to map this package.
  */
 public interface PackageMapper {}
